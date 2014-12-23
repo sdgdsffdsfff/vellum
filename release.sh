c3b3c312f0ef1cf8292e9ffa5cb9fef051a138f3
gloomyzerg <gloomyzerg@126.com>
2014-12-23 03:16:59 +0000

    修改配置文件和发布脚本

--- release.sh
+++ release.sh
@@ -3,4 +3,16 @@ echo '执行中...'
 rm -rf ./build/
 rm -rf ./node_modules/
 rm -rf ./public/bower_components/
+
+echo '' > config.json
+echo '{'                              > config.json
+echo '  "mysql": {'                   >> config.json
+echo '    "host": "localhost",'       >> config.json
+echo '    "port": "3306",'            >> config.json
+echo '    "user": "root",'            >> config.json
+echo '    "password": "password",'    >> config.json
+echo '    "database": "vellum"'       >> config.json
+echo '  }'                            >> config.json
+echo '}'                              >> config.json
+
 echo '完成'
